457142202ab88d58e1856b42bae2cf9df7ba7752
thomasblain <thomas.blain@uoit.net>
2021-11-10 13:03:38 -0500

    added a namespace, frequency slider, stop button

--- scripts/synth.js
+++ scripts/synth.js
@@ -1,13 +1,38 @@
-startup = function() {
-  const heading = document.getElementById('title');
-  heading.textContent = 'Shit Synth v0';
-}
+var synth = (function () {
 
-const audioCtx = new window.AudioContext();
+  var audioCtx;
+  var osc;
+  var frequency;
 
-playSound = function(){
-  const osc = audioCtx.createOscillator();
-  osc.frequency.setValueAtTime(440, audioCtx.currentTime);
-  osc.connect(audioCtx.destination);
-  osc.start();
-}
+  startup = function() {
+    const heading = document.getElementById('title');
+    heading.textContent = 'Shit Synth v0';
+    audioCtx = new AudioContext();
+    frequency = document.getElementById('frequencySlider').value;
+
+    const slider = document.getElementById('frequencySlider');
+    const minVal = document.getElementById('minVal');
+    minVal.textContent = slider.min + "hz";
+
+    const maxVal = document.getElementById('maxVal');
+    maxVal.textContent = slider.max + "hz";
+  }
+
+  playSound = function() {
+    osc = audioCtx.createOscillator();
+    osc.type = 'square';
+    osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+    osc.connect(audioCtx.destination);
+    osc.start();
+  }
+
+  updateTone = function() {
+    frequency = document.getElementById('frequencySlider').value;
+    osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+  }
+
+  stopSound = function() {
+    osc.stop();
+  }
+
+})();
