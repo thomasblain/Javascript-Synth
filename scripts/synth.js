3082312b0284ecd086a307f1809ddf498a8f143c
thomasblain <thomas.blain@uoit.net>
2021-11-11 13:21:31 -0500

    refactored layout in html and css, added panning and gain nodes, fixed synth replaying bug when clicking play multiple times

--- scripts/synth.js
+++ scripts/synth.js
@@ -3,32 +3,66 @@ var synth = (function () {
   var audioCtx;
   var osc;
   var frequency;
+  var panner;
+  var gain;
+  var started = false;
 
   // setup audio context and populate frequency slider labels
   startup = function() {
     const heading = document.getElementById('title');
-    heading.textContent = 'Shit Synth v0.1';
+    heading.textContent = 'Shit Synth v0.12';
 
     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     frequency = document.getElementById('frequencySlider').value;
 
-    const slider = document.getElementById('frequencySlider');
-    const minVal = document.getElementById('minVal');
-    minVal.textContent = slider.min + "hz";
-
-    const currentVal = document.getElementById('currentVal');
-    currentVal.textContent = frequency + "hz";
-
-    const maxVal = document.getElementById('maxVal');
-    maxVal.textContent = slider.max + "hz";
-  }
-
-  playSound = function() {
+    gain = audioCtx.createGain();
     osc = audioCtx.createOscillator();
     osc.type = document.getElementById('pattern').value;
     osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
-    osc.connect(audioCtx.destination);
-    osc.start();
+
+    panner = new StereoPannerNode(audioCtx);
+    panner.connect(audioCtx.destination);
+    panner.connect(gain);
+    gain.connect(audioCtx.destination);
+
+    const slider = document.getElementById('frequencySlider');
+    const minVal = document.getElementById('minHzVal');
+    minVal.textContent = slider.min + "hz";
+
+    const currentVal = document.getElementById('currentHzVal');
+    currentVal.textContent = frequency + "hz";
+
+    const maxVal = document.getElementById('maxHzVal');
+    maxVal.textContent = slider.max + "hz";
+
+    const panSlider = document.getElementById('panSlider');
+    const minPanVal = document.getElementById('minPanVal');
+    minPanVal.textContent = panSlider.min;
+
+    const currentPanVal = document.getElementById('currentPanVal');
+    currentPanVal.textContent = panSlider.value;
+
+    const maxPanVal = document.getElementById('maxPanVal');
+    maxPanVal.textContent = panSlider.max;
+
+    const gainSlider = document.getElementById('gainSlider');
+    const minGainVal = document.getElementById('minGainVal');
+    minGainVal.textContent = gainSlider.min;
+
+    const currentGainVal = document.getElementById('currentGainVal');
+    currentGainVal.textContent = gainSlider.value;
+
+    const maxGainVal = document.getElementById('maxGainVal');
+    maxGainVal.textContent = gainSlider.max;
+  }
+
+  playSound = function() {
+    if (started) {
+      osc.connect(panner);
+    } else {
+      osc.start();
+      started = true;
+    }
   }
 
   updateTone = function() {
@@ -36,7 +70,7 @@ var synth = (function () {
     if (osc) osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
 
     const slider = document.getElementById('frequencySlider');
-    const currentVal = document.getElementById("currentVal");
+    const currentVal = document.getElementById("currentHzVal");
     if (currentVal && slider) currentVal.textContent = slider.value + "hz";
   }
 
@@ -45,8 +79,24 @@ var synth = (function () {
     if (pattern) osc.type = pattern;
   }
 
+  updatePanner = function() {
+    const panValue = document.getElementById('panSlider').value;
+    panner.pan.value = panValue;
+
+    const currentPanVal = document.getElementById('currentPanVal');
+    currentPanVal.textContent = panValue;
+  }
+
+  updateGain = function() {
+    const gainValue = document.getElementById('gainSlider').value;
+    gain.gain.value = gainValue;
+
+    const currentGainVal = document.getElementById('currentGainVal');
+    currentGainVal.textContent = gainValue;
+  }
+
   stopSound = function() {
-    if (osc) osc.stop();
+    if (osc) osc.disconnect(panner);
   }
 
 })();
